d42e89e02ca035632c7c385881da6318e34f2176
Zarifa <mamadalievazara@gmail.com>
2026-02-07 01:11:25 +0100

    Add: partials to avoid code duplication for new and edit actions

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy]
+
   def index
     @products = Product.all
   end
 
   def show 
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -18,18 +19,31 @@ class ProductsController < ApplicationController
     else
       render :new, status: :unprocessable_entity 
     end
-
-    private 
-    def product_params
-      params.expect(product: [ :name ])
-    end
   end
 
   def edit 
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = 
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+
+  private 
+    def product_params
+      params.expect(product: [ :name ])
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
 end
