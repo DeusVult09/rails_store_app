f5a25982db632d583fc27275287364235b975276
Zarifa <mamadalievazara@gmail.com>
2026-02-08 00:09:31 +0100

    Add: rich text field and active storage fuction to upload files
    Add: internationalization to translate the page into diff. languages

--- store/app/controllers/application_controller.rb
+++ store/app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 end
