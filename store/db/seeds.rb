759fa6abfd8bcfb7f873fcf367126956a043d383
Zarifa <mamadalievazara@gmail.com>
2026-02-15 00:09:55 +0100

    Clear seeds.rb file, remove migrate initializer

--- store/db/seeds.rb
+++ store/db/seeds.rb
@@ -8,23 +8,4 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 # db/seeds.rb
-Product.find_or_create_by!(name: "T-shirt") do |p|
-  p.description = "T-shirts are essential when it's hot outside"
-  p.inventory_count = 33
-end
-
-Product.find_or_create_by!(name: "Shoes") do |p|
-  p.description = "Shoes are cool"
-  p.inventory_count = 1
-end
-
-Product.find_or_create_by!(name: "Denim") do |p|
-  p.description = "The best creation of Americans"
-  p.inventory_count = 4
-end
-
-Product.find_or_create_by!(name: "Pants") do |p|
-  p.description = "Database fundamentals"
-  p.inventory_count = 12
-end
 
